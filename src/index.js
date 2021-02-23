daca5bfddb0eb723d8348f5a723328a5ae95bea1
github username <scott.gloyna01@gmail.com>
2021-02-23 17:23:04 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  function addTask(event) {
+    let newTaskText = document.getElementById('new-task-description').value;
+    let taskList = document.getElementById('list')
+    let newTaskItem = document.createElement('li');
+    newTaskItem.innerText = newTaskText
+    taskList.append(newTaskItem);
+    let background = document.getElementById("main-content").style.backgroundColor = getRandomColor()
+    event.preventDefault();
+  }
+
+
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addTask)
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 });
