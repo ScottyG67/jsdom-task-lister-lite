5096cd728dba3483eef43fcd4df62787fc623a81
github username <scott.gloyna01@gmail.com>
2021-02-24 15:40:01 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   function addTask(event) {
-    let newTaskText = document.getElementById('new-task-description').value;
-    let taskList = document.getElementById('list')
+    event.preventDefault();
+    let newTaskText = event.new-task-description.value;
+    let taskList = event.list
     let newTaskItem = document.createElement('li');
     newTaskItem.innerText = newTaskText
     taskList.append(newTaskItem);
-    let background = document.getElementById("main-content").style.backgroundColor = getRandomColor()
-    event.preventDefault();
+    //let background = document.getElementById("main-content").style.backgroundColor = getRandomColor()
+    
   }
 
 
